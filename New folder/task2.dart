b06b2158161a5f5025601a27a6ec2dc8ce1ad18a
mariamabass <115428727+mariamabass@users.noreply.github.com>
2022-11-07 12:01:21 -0800

    updat 1

---
+++ New folder/task2.dart
@@ -0,0 +1,19 @@
+import 'dart:io' ;
+void main (){
+    List values =[];
+print ("please enter the length of the list");
+int? n = int. parse (stdin.readLineSync()!);
+
+int max = -99999999;
+int mini= 9999999;
+
+for (int i = 0 ; i<n; i++){
+  int m = int.parse(stdin.readLineSync()!);
+   if(m >max ){
+    max=m ;
+}
+if(m < mini){
+    mini=m;
+}
+}
+}
